033f1b503b018352643e5ae5d84dc9c2023594fb
¨Adrian <Adrian.Cooke@utwente.nl>
2012-10-11 11:34:10 +0200

    Creating a class to handle experiment data.

---
+++ classes/Experiment.m
@@ -0,0 +1,12 @@
+classdef Experiment < handle
+    %EXPERIMENT Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
