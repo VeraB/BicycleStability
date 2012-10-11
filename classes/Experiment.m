044cfc14efa7bca705cabcfaefae6bffe5019302
¨Adrian <Adrian.Cooke@utwente.nl>
2012-10-11 16:00:01 +0200

    Added function to read in AR data and sync with IMU frame sensor.

--- classes/Experiment.m
+++ classes/Experiment.m
@@ -1,10 +1,21 @@
 classdef Experiment < handle
     %EXPERIMENT A class to handle the processing and configuration of
     %experiment runs
+    properties(Constant)
+       ARRUNEXTENSION='/ar';
+       IMURUNEXTENSION='/imu';
+       %PLOT STYLES
+       STEERINGANGLEPLOTSTYLE = '--r.';
+       ARPLOTSTYLE = '--g.';
+       FRAMESENSORPLOTSYLE = '--m.';
+
+    end
     properties
         %Configuration
         cacheTheReadData=true
         Fs_IMU=200;
+        Fs_AR=30;
+        Variance_AR = 0;
         callibrateStart=200;
         plotTheSteeringAndRollAngleRuns=false;
         filename='';
@@ -22,7 +33,11 @@ classdef Experiment < handle
         steeringColumnSyncs = [];
         frameRun= [];
         frameRunSyncs = [];
+        arFrontRun = [];
+        arFrontRunSyncs = [];
         %Processed Data.
+        filenamePrint='';
+        runNamePrint='';
         bicycleSteeringAngle=[];
         bicycleRollAngle = [];
         bicycleSpeed = [];
@@ -44,13 +59,82 @@ classdef Experiment < handle
         function ex = processRun(ex,runName)
             %Process a single RUN.
             ex.runName=runName;
+            ex.runNamePrint = strrep(ex.runName,'/','-');
+            ex.runNamePrint = strrep(ex.runNamePrint,'_','-');
+            ex.filenamePrint = strrep(ex.filename,'/','-');
+            ex.filenamePrint = strrep(ex.filenamePrint,'_','-');
             ex = ex.getVelocityRollAndSteeringAngles();
+            ex = ex.getARDataAndSyncWithIMU();
+        end
+        
+        function [ex] = getARDataAndSyncWithIMU(ex)
+            loadedDataFilename=['./dataloaded' ex.runNamePrint ex.filenamePrint...
+                '-getARDataAndSyncWithIMU.mat'];
+            %Get the data
+            display(['GETTING DATA FROM FILE:' loadedDataFilename]);
+            %Only load from file if caching is enabled.
+            if ((ex.cacheTheReadData)&&(exist(loadedDataFilename,'file')))
+                load(loadedDataFilename);
+            else
+              %Get the AR data
+                    runNameAR= [ex.runName ex.ARRUNEXTENSION];
+                    [ex.arFrontRun,ex.arFrontRunSyncs] = ...
+                        Quat3D.readDataPromove(ex.filename,runNameAR,...
+                        -1,ex.Fs_IMU,ex.Fs_IMU);
+            end
+            display('DATA RECEIVED');
+            changedArFrontRun = ThreeD.changeStartTime(ex.arFrontRun,0);
+            changedFrameRun = ThreeD.changeStartTime(ex.frameRun,0);
+            [roll_frame,pitch_frame,yaw_frame,t_ar,figureARInverse]=ThreeD.getAndPlotRPYt(...
+                changedArFrontRun,'RAW AR/INVERSE DATA',...
+                false,'timeseries',ex.ARPLOTSTYLE);
+            [ex.Fs_AR,ex.Variance_AR] =  ThreeD.estimateFsAndVariance(t_ar);
+            %Get the inverse function - Bike moves not global frame.
+            changedArFrontRun =ThreeD.cellTranspose(changedArFrontRun);
+            [roll_frame,pitch_frame,yaw_frame,t_ar]=ThreeD.getAndPlotRPYt(...
+                changedArFrontRun,'',...
+                figureARInverse,'timeseries',ex.FRAMESENSORPLOTSYLE);
+            %Get change of zero frame
+            imuToAr = ThreeD.getChangeOfGlobalReferenceFrames(...
+               changedArFrontRun,changedFrameRun,ex.callibrateStart,10);
+            changedFrameRun = imuToAr*changedFrameRun;
+            [roll_frame,pitch_frame,yaw_frame,t_frame,figureResampled]=ThreeD.getAndPlotRPYt(...
+                changedFrameRun,'IMU CHANGED TO AR',...
+                false,'timeseries',ex.FRAMESENSORPLOTSYLE);
+            
+            
+            display('RESAMPLE')
+            N=length(t_ar);
+            tStart = t_ar(1);
+            tEnd= t_ar(N);
+            t_required = (0:1/ex.Fs_AR:tEnd-tStart);
+            changedFrameRun = ThreeD.resample(changedFrameRun,t_required);
+            changedArFrontRun = ThreeD.resample(changedArFrontRun,t_required);
+            
+            [roll_ar,pitch_ar,yaw_ar,t_ar,figureResampled]=ThreeD.getAndPlotRPYt(...
+                changedArFrontRun,'AR/frame sensor RESAMPLED',...
+                false,'timeseries',ex.ARPLOTSTYLE);
+            [roll_frame,pitch_frame,yaw_frame,t_frame,figureResampled]=ThreeD.getAndPlotRPYt(...
+                changedFrameRun,'',...
+                figureResampled,'timeseries',ex.FRAMESENSORPLOTSYLE);
+            
+            display('SYNCHRONISE IMU WITH AR')
+            [changedArFrontRun,changedFrameRun] = synchroniseWithRespectToRPY(...
+                roll_ar,pitch_ar,yaw_ar,changedArFrontRun,...
+                roll_frame,pitch_frame,yaw_frame,changedFrameRun,1);
+            [roll_ar,pitch_ar,yaw_ar,t_ar,figureSynced]=ThreeD.getAndPlotRPYt(...
+                changedArFrontRun,'AR/frameSensor synchronised',...
+                false,'timeseries',ex.ARPLOTSTYLE);
+            [roll_frame,pitch_frame,yaw_frame,t_frame,figureSynced]=ThreeD.getAndPlotRPYt(...
+                changedFrameRun,'',...
+                figureSynced,'timeseries',ex.FRAMESENSORPLOTSYLE);
         end
         
         function [ex] = getVelocityRollAndSteeringAngles(ex)
             %getVelocityRollAndSteeringAngles 
             %Processes a run in an experiment and calculates the
-            %steering and roll angles with the plots as well.
+            %steering and roll angles with the plots as well, only using
+            %the IMU and ANT sensors
             % RETURNS:
             % bicycleSteeringAngle - The Steering angle for the run.
             % bicycleRollAngle - The roll angle for the run.
@@ -59,7 +143,6 @@ classdef Experiment < handle
             % ant_t - The time vector for the speed and cadence measurements.
             % figures - A list of the figures handles, in case you want to export
             % by using laprint or something.
-            finalPlotStyle = '--r.';
             display(['%%%%%%%%%%%%%%Processing RUN:' ex.runName ' %%%%%%%%%%%%%%'])
             display('CREATING FIGURES')
             if isempty(ex.figures)
@@ -81,13 +164,9 @@ classdef Experiment < handle
                 figSpeed = ex.figures(6);
             end
             display('FIGURES CREATED')
-            
-            runNamePrint = strrep(ex.runName,'/','-');
-            runNamePrint = strrep(runNamePrint,'_','-');
-            filenamePrint = strrep(ex.filename,'/','-');
-            filenamePrint = strrep(filenamePrint,'_','-');
-            loadedDataFilename=['./dataloaded' runNamePrint filenamePrint...
-                '-getbicycleangles.mat'];
+            loadedDataFilename=['./dataloaded' ex.runNamePrint ...
+                ex.filenamePrint...
+                '-getVelocityRollAndSteeringAngles.mat'];
             %Get the data
             display(['GETTING DATA FROM FILE:' loadedDataFilename]);
             %Only load from file if caching is enabled.
@@ -96,12 +175,14 @@ classdef Experiment < handle
             else
                 if ex.processIMUorAdams==true
                     %Read promove with no resampling
+                    runNameIMU= [ex.runName ex.IMURUNEXTENSION];
                     [ex.steeringColumnRun,ex.steeringColumnSyncs] = ...
-                        Quat3D.readDataPromove(ex.filename,ex.runName,...
+                        Quat3D.readDataPromove(ex.filename,runNameIMU,...
                         ex.steeringImuNodeId,ex.Fs_IMU,ex.Fs_IMU);
                     [ex.frameRun,ex.frameRunSyncs] = ...
-                        Quat3D.readDataPromove(ex.filename,ex.runName,...
+                        Quat3D.readDataPromove(ex.filename,runNameIMU,...
                         ex.rollingImuNodeId,ex.Fs_IMU,ex.Fs_IMU);
+                    %Get the ANT data (forward speed)
                     theAntReader = antReader(ex.filename,ex.runName);
                     [ex.bicycleSpeed,ex.bicycleCadence,ex.ant_t,...
                         ex.ant_sync,ex.ant_sync_t] = ...
@@ -189,7 +270,7 @@ classdef Experiment < handle
             ThreeD.plotRPY(ex.bicycleRollAngle,...
                 frameSensorPitchAngle,...
             ex.bicycleSteeringAngle,...
-                timestamps,true,'timeseries',finalPlotStyle);
+                timestamps,true,'timeseries',ex.STEERINGANGLEPLOTSTYLE);
             
             if ex.plotTheSteeringAndRollAngleRuns==1
                 display('PLOT STEERING QUAT');
